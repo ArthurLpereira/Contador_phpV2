4bd40a8bfc6185b6368975c29b8f13578c010140
EricJr05 <eric.j.oliveira@aluno.senai.br>
2025-05-14 15:45:35 -0300

    Add files via upload

--- back-end/endpoints/post_user.php
+++ back-end/endpoints/post_user.php
@@ -6,20 +6,27 @@ header('Content-Type: application/json');
 $dados = json_decode(file_get_contents('php://input'), true);
 
 if (isset($dados['nome'], $dados['senha'], $dados['confirmacao'])) {
-    $nome = $mysqli->real_escape_string($dados['nome']);
+    $nome = trim($dados['nome']);
     $senha = $dados['senha'];
     $confirmacao = $dados['confirmacao'];
 
     if ($senha === $confirmacao) {
         $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
-        $sql = "INSERT INTO usuarios (nome_usuario, senha_usuario, nivel_usuario, ativo_usuario) 
-                VALUES ('$nome', '$senhaHash', '1', '1')";
-        $query = $mysqli->query($sql);
 
-        if ($query && $mysqli->affected_rows > 0) {
-            echo json_encode(['mensagem' => 'Usuário cadastrado com sucesso']);
+        $stmt = $mysqli->prepare("INSERT INTO usuarios (nome_usuario, senha_usuario, nivel_usuario, ativo_usuario) VALUES (?, ?, 1, 1)");
+        if ($stmt) {
+            $stmt->bind_param("ss", $nome, $senhaHash);
+            $executado = $stmt->execute();
+
+            if ($executado && $stmt->affected_rows > 0) {
+                echo json_encode(['mensagem' => 'Usuário cadastrado com sucesso']);
+            } else {
+                echo json_encode(['erro' => 'Erro ao cadastrar o usuário']);
+            }
+
+            $stmt->close();
         } else {
-            echo json_encode(['erro' => 'Erro ao cadastrar o usuário']);
+            echo json_encode(['erro' => 'Erro ao preparar a query']);
         }
     } else {
         echo json_encode(['erro' => 'As senhas não coincidem']);
--- front-end/view/calendario.php
+++ front-end/view/calendario.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Login</title>
 </head>
 
@@ -33,7 +35,7 @@
                     <li>
                         <a href="relatorio.php">Contagem</a>
                     </li>
-                    <li><a href="index.php">Sair</a></li>
+                    <li><a href="/../../">Sair</a></li>
                 </ul>
             </nav>
         </div>
